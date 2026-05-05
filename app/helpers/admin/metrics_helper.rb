ea24ad64d0e655779989f4d34a69c2e3e6f8ccff
Koji5 <sakamoto5002@gmail.com>
2026-05-05 15:57:42 +0900

    Unify datetime and duration display formats (#863)

--- app/helpers/admin/metrics_helper.rb
+++ app/helpers/admin/metrics_helper.rb
@@ -1,18 +1,4 @@
 # frozen_string_literal: true
 
 module Admin::MetricsHelper
-  def seconds_to_human_time(seconds)
-    s = seconds.to_i
-    return "0分" if s <= 0
-    return "1分未満" if s < 60
-
-    h = s / 3600
-    m = (s % 3600) / 60
-
-    if h.positive?
-      "#{h}時間#{m}分"
-    else
-      "#{m}分"
-    end
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,21 @@
 require "uri"
 
 module ApplicationHelper
+  def seconds_to_human_time(seconds)
+    s = seconds.to_i
+    return "0分" if s <= 0
+    return "1分未満" if s < 60
+
+    h = s / 3600
+    m = (s % 3600) / 60
+
+    if h.positive?
+      "#{h}時間#{m}分"
+    else
+      "#{m}分"
+    end
+  end
+
   def safe_external_url(url)
     value = url.to_s.strip
     return nil if value.blank?
