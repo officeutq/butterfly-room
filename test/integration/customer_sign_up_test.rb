819230ba7b4d2426a6538e6128b94f922ab9fff9
Koji5 <245330306+officeutq@users.noreply.github.com>
2026-09-12 14:23:48 +0900

    ウェルカムページの店舗向け導線とポイント案内を調整

--- test/integration/customer_sign_up_test.rb
+++ test/integration/customer_sign_up_test.rb
@@ -3,13 +3,24 @@
 require "test_helper"
 
 class CustomerSignUpTest < ActionDispatch::IntegrationTest
-  test "guest welcome shows login and sign_up links" do
+  test "guest welcome shows account and store links without point prices or purchase button" do
     get welcome_path
     assert_response :success
 
-    assert_select "a", text: "ログイン", href: new_user_session_path
-    assert_select "a", text: "視聴者アカウント 新規作成", href: sign_up_path
-    assert_select "a", text: "店舗向けページを見る", href: stores_lp_path
+    assert_select ".guest-home" do
+      assert_select "a.btn-lg[href=?]", new_user_session_path, text: "ログイン", count: 1
+      assert_select "a.btn-lg[href=?]", sign_up_path, text: "視聴者アカウント 新規作成", count: 1
+      assert_select "a.btn-lg[href=?]", stores_lp_202609_path, text: "店舗向けサービスを見る", count: 2
+      assert_select "a[href=?]", stores_lp_path, count: 0
+      assert_select "a", text: "店舗向けページを見る", count: 0
+      assert_select "a", text: "ポイントを購入する", count: 0
+      assert_select "div", text: /\A[\d,]+pt：[\d,]+円（税込）\z/, count: 0
+      assert_select "p", text: "ポイント購入に関して", count: 1
+      assert_select "h2", text: "視聴者は、配信をドリンクで応援できます。", count: 1
+      assert_select "p", text: "Butterflyveでは、購入したポイントを使ってライブ配信中のキャストへドリンクを送信できます。", count: 1
+      assert_select "p", text: "※本サービスは会員制サービスです。ポイントの購入および利用にはログインが必要です。", count: 1
+      assert_select "h3", text: "店舗の方へ", count: 1
+    end
   end
 
   test "signed in user is redirected from welcome to the normal home" do
